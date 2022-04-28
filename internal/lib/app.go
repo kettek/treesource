0ef6b3d73e52d2cd8cf702cf9c6a72650d49e9c6
kts of kettek <kts@kettek.net>
2022-04-28 07:23:44 -0700

    Add DirView

--- internal/lib/app.go
+++ internal/lib/app.go
@@ -5,11 +5,14 @@ import (
 	"io/fs"
 	"os"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 // App struct
 type App struct {
-	ctx context.Context
+	ctx   context.Context
+	views []*DirView
 }
 
 // NewApp creates a new App application struct
@@ -55,4 +58,28 @@ func (a *App) ListFiles(loc string) (entries []DirEntry, err error) {
 		entries = append(entries, de)
 	}
 	return
+}
+
+func (a *App) NewDirView() uuid.UUID {
+	dv := &DirView{
+		UUID: uuid.New(),
+	}
+	a.views = append(a.views, dv)
+	return dv.UUID
+}
+
+func (a *App) RemoveDirView(u uuid.UUID) {
+	for i, dv := range a.views {
+		match := true
+		for i, v := range u {
+			if dv.UUID[i] != v {
+				match = false
+			}
+		}
+		if !match {
+			continue
+		}
+		a.views = append(a.views[:i], a.views[i+1:]...)
+		break
+	}
 }
