81d410f1482e5c4c542806e288f1b8082a1847bd
kts of kettek <kts@kettek.net>
2022-04-28 20:02:48 -0700

    Add closePopup

--- frontend/src/menu/Menus.svelte
+++ frontend/src/menu/Menus.svelte
@@ -22,11 +22,18 @@
       currentPopup.set({name: popup, x, y})
       currentSubPopup.set({name: '', x, y})
     },
+    closePopup: () => {
+      currentPopup.set({name: '', x: 0, y: 0})
+      currentSubPopup.set({name: '', x: 0, y: 0})
+    },
     currentPopup,
     openSubPopup: (popup: string, x: number, y: number) => {
       x = Math.floor(x)
       y = Math.floor(y)
       currentSubPopup.set({name: popup, x, y})
+    },
+    closeSubPopup: () => {
+      currentSubPopup.set({name: '', x: 0, y: 0})
     },
     currentSubPopup,
   })
