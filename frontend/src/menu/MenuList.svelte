487a79ce16fd1c73663dc5a2eea41a66b623dd75
kts of kettek <kts@kettek.net>
2022-04-28 19:48:06 -0700

    Add menu list transition and some styling

--- frontend/src/menu/MenuList.svelte
+++ frontend/src/menu/MenuList.svelte
@@ -2,6 +2,7 @@
   import { getContext } from 'svelte'
   import { POPUPS } from './Menus.svelte'
   import { clickOutside } from './clicker'
+  import { fade } from 'svelte/transition'
 
   export let popup: string = ''
   export let subpopup: string = ''
@@ -9,11 +10,11 @@
 </script>
 
 {#if popup && $currentPopup.name === popup}
-  <nav style="left: {$currentPopup.x}px; top: {$currentPopup.y}px;" use:clickOutside on:outclick={()=>openPopup('', 0, 0)}>
+  <nav transition:fade="{{duration:100}}" style="left: {$currentPopup.x}px; top: {$currentPopup.y}px;" use:clickOutside on:outclick={()=>openPopup('', 0, 0)}>
     <slot></slot>
   </nav>
 {:else if subpopup && $currentSubPopup.name === subpopup}
-  <nav style="left: {$currentSubPopup.x}px; top: {$currentSubPopup.y}px;" use:clickOutside on:outclick={()=>openSubPopup('', 0, 0)}>
+  <nav transition:fade="{{duration:100}}" style="left: {$currentSubPopup.x}px; top: {$currentSubPopup.y}px;" use:clickOutside on:outclick={()=>openSubPopup('', 0, 0)}>
     <slot></slot>
   </nav>
 {/if}
@@ -23,5 +24,6 @@
     position: fixed;
     display: grid;
     grid-template-columns: minmax(0, 1fr);
+    box-shadow: 3px 3px 0px 1px teal;
   }
 </style>
