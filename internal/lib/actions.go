516edeb14ced442cc3aa7c03043c487636ca3b2b
kts of kettek <kts@kettek.net>
2022-05-03 00:18:00 -0700

    Add actions for add and remove dir

---
+++ internal/lib/actions.go
@@ -0,0 +1,103 @@
+package lib
+
+import (
+	"fmt"
+	"treesource/internal/do"
+)
+
+type AddDirectoryAction struct {
+	// Full directory state!
+	Directory Directory
+	Index     int
+}
+
+func (a *AddDirectoryAction) Apply(p *Project) {
+	fmt.Println("action: apply add dir")
+	if len(p.Directories) == a.Index {
+		p.Directories = append(p.Directories, *a.Directory.Clone())
+	} else {
+		p.Directories = append(p.Directories[:a.Index+1], p.Directories[a.Index:]...)
+		p.Directories[a.Index] = *a.Directory.Clone()
+	}
+	p.Emit(EventDirectoryAdd, DirectoryAddEvent{
+		UUID: a.Directory.UUID,
+		Path: a.Directory.Path,
+	})
+	// Need to rehook garbage.
+	p.InitDirectory(&p.Directories[a.Index])
+	// Seems reasonable enough to emit all entries on load.
+	p.Directories[a.Index].EmitAllEntries()
+}
+
+func (a *AddDirectoryAction) Unapply(p *Project) {
+	fmt.Println("action: unapply add dir")
+	p.Directories = append(p.Directories[:a.Index], p.Directories[a.Index+1:]...)
+	p.Emit(EventDirectoryRemove, DirectoryRemoveEvent{
+		UUID: a.Directory.UUID,
+	})
+}
+
+type RemoveDirectoryAction struct {
+	// Full directory state!
+	Directory Directory
+	Index     int
+}
+
+func (a *RemoveDirectoryAction) Apply(p *Project) {
+	fmt.Println("action: apply remove dir")
+	for i, d := range p.Directories {
+		if d.UUID.String() == a.Directory.UUID.String() {
+			p.Directories = append(p.Directories[:i], p.Directories[i+1:]...)
+			p.Emit(EventDirectoryRemove, DirectoryRemoveEvent{
+				UUID: d.UUID,
+			})
+			break
+		}
+	}
+}
+
+func (a *RemoveDirectoryAction) Unapply(p *Project) {
+	fmt.Println("action: unapply remove dir")
+	if len(p.Directories) == a.Index {
+		p.Directories = append(p.Directories, *a.Directory.Clone())
+	} else {
+		p.Directories = append(p.Directories[:a.Index+1], p.Directories[a.Index:]...)
+		p.Directories[a.Index] = *a.Directory.Clone()
+	}
+	p.Emit(EventDirectoryAdd, DirectoryAddEvent{
+		UUID: a.Directory.UUID,
+		Path: a.Directory.Path,
+	})
+	// Need to rehook garbage.
+	p.InitDirectory(&p.Directories[a.Index])
+	// Seems reasonable enough to emit all entries on load.
+	p.Directories[a.Index].EmitAllEntries()
+}
+
+type SyncDirectoryAction struct {
+	// State of added or removed directory files.
+}
+
+func (a *SyncDirectoryAction) Apply(p *Project) {
+	fmt.Println("action: apply sync dir")
+}
+
+func (a *SyncDirectoryAction) Unapply(p *Project) {
+	fmt.Println("action: unapply sync dir")
+}
+
+type GroupedAction struct {
+	Actions []do.Action[*Project]
+}
+
+func (a *GroupedAction) Apply(p *Project) {
+	for _, a2 := range a.Actions {
+		a2.Apply(p)
+	}
+}
+
+func (a *GroupedAction) Unapply(p *Project) {
+	for i := len(a.Actions); i > 0; i-- {
+		a.Actions[i].Unapply(p)
+	}
+}
