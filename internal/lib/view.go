954d2a39c365d5994c0981bc0da13e330d349f06
kts of kettek <kts@kettek.net>
2022-05-04 00:04:29 -0700

    Add view types

---
+++ internal/lib/view.go
@@ -0,0 +1,13 @@
+package lib
+
+import "github.com/google/uuid"
+
+type DirectoryView struct {
+	Directory uuid.UUID `json:"directory" yaml:"directory"`
+	WD        string    `json:"wd" yaml:"wd"`
+}
+
+type TagsView struct {
+	UUID uuid.UUID `json:"uuid" yaml:"uuid"`
+	Tags []string  `json:"tags" yaml:"tags"`
+}
