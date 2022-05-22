dc3ab10b371efd6d63bffb0ea21356c24d397b20
kts of kettek (POWERQWACK) <kts@kettek.net>
2022-05-21 23:17:47 -0700

    Add throbber

---
+++ frontend/src/components/Throbber.svelte
@@ -0,0 +1,28 @@
+<main>LOADING</main>
+
+<style>
+  main {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    animation: throb 10s linear, fadein 500ms;
+  }
+  @keyframes throb {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes fadein {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
